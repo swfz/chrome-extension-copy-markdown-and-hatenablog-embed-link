3dfba4ac6de374e539c7447f50384e6ee0d33db0
swfz <sawafuji.09@gmail.com>
2022-08-24 19:52:37 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+function copy(str: string) {
+  const listener = (event: any) => {
+    event.clipboardData.setData("text/plain", str);
+    event.preventDefault();
+  };
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+}
+
+// [bookmarklets/bookmarklets at master · swfz/bookmarklets](https://github.com/swfz/bookmarklets/tree/master/bookmarklets)
+// [https://github.com/swfz/bookmarklets/tree/master/bookmarklets:embed:cite]
+chrome.tabs.query(
+  {
+    active: true,
+    currentWindow: true,
+  },
+  (tabs) => {
+    const { title, url } = tabs[0];
+    copy(
+      `[${title}](${url})\n\n[${url}:embed:cite]`
+    );
+  }
+)
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+// Node 14 & 16
+import manifest from './manifest.json'
+// Node >=17
+// import manifest from './manifest.json' assert { type: 'json' }
+
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+});
